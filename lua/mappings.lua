4e6c83c44a78d28f14e6526b54244e614e556a62
Se7s <hx3000se7s@gmail.com>
2025-06-12 23:48:12 +0300

    Added Terminal Support and Improved Right Click Menu

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -8,9 +8,23 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
-map({ "n", "i" }, "<C-t>", function()
-  require("menu").open('default')
-end, { desc = "Menu" })
+-- Keyboard users
+vim.keymap.set("n", "<C-t>", function()
+  require("menu").open("default")
+end, { desc = "Right Click Menu" })
+
+-- mouse users + nvimtree users!
+vim.keymap.set({ "n", "v" }, "<RightMouse>", function()
+  require('menu.utils').delete_old_menus()
+
+  vim.cmd.exec '"normal! \\<RightMouse>"'
+
+  -- clicked buf
+  local buf = vim.api.nvim_win_get_buf(vim.fn.getmousepos().winid)
+  local options = vim.bo[buf].ft == "NvimTree" and "nvimtree" or "default"
+
+  require("menu").open(options, { mouse = true })
+end, { desc = "Right Click Menu" })
 
 map("n", "<M-f>", "<cmd> FZF <cr>", { desc = "FZF File Finder" })
 
@@ -33,3 +47,6 @@ map("n", "<leader>r", function()
     end
   }
 end)
+
+
+map("n", "<leader>t", "<cmd> terminal <cr>")
